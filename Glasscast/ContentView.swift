a03821c00407f6141600f8eb99d29c8967e66bed
yogendra jangid <yogendrajangid902@gmail.com>
2026-01-19 21:42:06 +0530

    Initial Commit

---
+++ Glasscast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glasscast
+//
+//  Created by Yogendra Ronalia on 19/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by Yogendra Ronalia on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
